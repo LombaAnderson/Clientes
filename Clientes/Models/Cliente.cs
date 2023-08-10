615679e961afc31289c4f07da781626e044d08b2
Anderson Lomba <andersonlomba172@outlook.com>
2023-08-10 19:52:12 -0300

    Adicionar arquivos de projeto.

---
+++ Clientes/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace Clientes
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Endereco { get; set; }
+
+
+    }
+}
